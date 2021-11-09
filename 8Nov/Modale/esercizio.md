cbeaa72c365e6b801ac233ac82ff3af80ff28714
silvia.hth <silvia.hth@gmail.com>
2021-11-09 19:10:02 +0100

    prova esercizio 8Nov

--- 8Nov/Modale/esercizio.md
+++ 8Nov/Modale/esercizio.md
@@ -19,3 +19,35 @@ Sulla base dell'esercizio 2. andate a gestire anche il contenuto (si intende il 
 che l'utente possa inserirne tutti quelli che vuole. Il funzionamento dovrà simulare in tutto quello di
 un dizionario/rubrica gestendo il flusso con i nuovi concetti di Parent / Children (padre / figlio)
 visti oggi.
+
+
+------------------------------------------
+
+sulla base dell'esercizio della modale, il bottone che prima non faceva nulla
+adesso deve ripulire tutto quello che sta sotto alla modale, così da lasciare
+uno spazio bianco.
+La modale di prima si chiude.
+
+             Nella schermata bianca, dopo 5 secondi comparirà un'altra modale, che avvisa
+             l'utente di non aver accettato la politica per accedere al sito e che (in via del tutto scherzosa)
+             una equipe di scimmie assassine usciranno fuori dal monitor.
+
+             Dopo 5 secondi dall'avviso, ogni singolo nome delle scimmie (e i relativi AKA), comparirà a centro dello schermo.
+             Ad intervalli, il nome cambia. In modo da far spaventare ancora di più l'utente.
+
+             Requisiti:
+                - qualunque listener o timeout deve essere ripulito, il core (browser) di JS non deve essere
+                  compromesso in alcun modo. (tutto deve scorrere fluidissimo, come sempre!)
+
+                - (mi sto girando a terra dalle risate... buon divertimento, :lol)
+
+```javascript
+// La lista delle scimmie assassine:
+const monkeysEquipe = [
+  { name: "John", aka: "lo spaventa passeri", surname: "Ross" },
+  { name: "Ector", aka: "il corsaro", surname: "Mauss" },
+  { name: "Franco", aka: "ciccio bello", surname: "La croix" },
+  { name: "Mika", aka: "la regina del male", surname: "Peretti" },
+  { name: "Lucy", aka: "il male", surname: "sconosciuto" },
+];
+```
--- 8Nov/Modale/script.js
+++ 8Nov/Modale/script.js
@@ -21,15 +21,6 @@ const h1Text = document.querySelector (".testo");
 
 // by prompt
 
-/*   h1RenameBtn.addEventListener(
-    "click",
-    () => { 
-            const h1TextPrompt = prompt ('inserisci il testo');   
-            h1Tag.textContent = h1TextPrompt;}
-,
-  );*/
-
-// by input
 
 h1RenameBtn.addEventListener("click", renameH1);
 
@@ -52,6 +43,34 @@ liText.addEventListener("keypress", (event) =>
 
 // ------------------------ Modale comincia da qui ------------------------
 
+const body = document.querySelector ('body')
+const main = document.querySelector ('main')
+const modal = document.querySelector(".modal");
+const acceptBtn = document.querySelector("#btn-accept");
+const denyBtn = document.querySelector("#btn-deny");
+
+// const addNewModal = () =>{
+//   let NewModal =document.createElement("div");
+//   NewModal.textContent = p.value}
+
+let newModal = document.createElement("div")
+const newContent = document.createTextNode("wrong");
+
+function threatenModal(contenitor){
+  let img = document.createElement("img");
+  let h3 = document.createElement('h3');
+  let p = document.createElement('p');
+  toggleClass(img, 'modal-img')
+  toggleClass(h3, 'modal-h3')
+  toggleClass(p, 'modal-p')
+  img.setAttribute('src', "https://img.icons8.com/ios/50/000000/harambe-the-gorilla--v1.png");
+  h3.textContent = `Sbagliato`;
+  p.textContent = `Le scimmie ti attaccheanno`;
+  contenitor.appendChild(img);
+  contenitor.appendChild(h3);
+  contenitor.appendChild(p);
+}
+
 function hideModal() {
     modal.style.opacity = 0;
   
@@ -60,23 +79,45 @@ function hideModal() {
     }, 350);
   }
   
-//   function redirectToGoogle() {
-//     window.location.href = "http://google.com";
-//   }
+const toggleClass = (target, newClass) => {
+    target.classList.toggle(newClass);
+  }
 
-function loadModal() {document.body.removeChild(modal);
+function removeElement (element) {
+  document.body.removeChild(element)
+  }
+
+function addElement (element) {
+    setTimeout(() => {
+      document.body.append(element);;
+    }, 5000);}
+
+
+function loadModal() {
+    document.body.removeChild(modal);
     setTimeout(() => {
         document.body.append(modal);;
-      }, 10000);}
-  
-  const modal = document.querySelector(".modal");
-  const acceptBtn = document.querySelector("#btn-accept");
-  const denyBtn = document.querySelector("#btn-deny");
-  
-  acceptBtn.addEventListener("click", hideModal, { once: true });
-//   denyBtn.addEventListener("click", redirectToGoogle, { once: true });
+      }, 1000);}
 
   document.addEventListener("DOMContentLoaded", loadModal)
   
+
+  
+  acceptBtn.addEventListener("click", hideModal, { once: true });
+
+
+
+  denyBtn.addEventListener("click", ()=>{
+    removeElement(main);
+    removeElement(modal);
+    addElement(newModal);
+    toggleClass(newModal, 'modal')
+    threatenModal(newModal);
+
+  });
+
+
+
+
   // ------------------------ Modale fine ------------------------
   
